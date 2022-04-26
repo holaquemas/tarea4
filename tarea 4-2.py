1f673607d6564f00085768acd44fb53830aa89e5
holaquemas <86824890+holaquemas@users.noreply.github.com>
2022-04-26 16:28:42 -0500

    Add files via upload

---
+++ tarea 4-2.py
@@ -0,0 +1,15 @@
+lista=[]
+
+for  num  in range (1,101):
+   lista.append(num)
+   listaR=lista[::-1]
+print("Lista orden Normal")
+print(lista, end=" ")
+print("")
+print("")
+print("Lista orden inverso")
+
+print(listaR, end=" ")
+    
+  
+   
